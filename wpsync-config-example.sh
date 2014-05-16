cea6ac5bcfffa7ebfcda827b64da44bf2453732c
wolfgangschoeffel <info@wolfgang-schoeffel.de>
2014-05-16 10:16:23 +0200

    initial commit

---
+++ wpsync-config-example.sh
@@ -0,0 +1,17 @@
+r_user= # remote username
+r_host= # remote host
+r_web_dir= # remote directory wher wordpress is stored
+
+r_web_addr= # remote website address http://example.com
+l_web_addr= # local website address for example http://example.local
+
+l_db_name= # local database name
+l_db_pass= # local database password
+l_db_user= # local database user
+
+r_db_name= # remote database name
+r_db_user= # remote database user
+r_db_pass= # remoate database password
+
+l_upload_dir= # local wordpress upload folder wp-content/uploads/ (relative to l_web_dir)
+r_upload_dir= # remote wordpress upload folder wp-content/uploads/ (relative to r_web_dir)
